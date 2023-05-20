1c4f6982d89456ef7e940dcf64ed284081837cbb
min-bok <leehy0782@gmail.com>
2023-05-20 19:09:20 +0900

    오현고 수업자료 최종

---
+++ js/memo.js
@@ -0,0 +1,10 @@
+// DOM 요소
+
+// .new-btn 클릭시, .modal-wrap의 hidden 클래스 없애기
+
+// .close-btn 클릭시, .modal-wrap에 hidden 클래스 추가
+// 힌트) remove대신 add 사용하기
+
+// .open-btn 클릭시, .modal-wrap의 hidden 클래스 없애기
+
+// .delete-btn 클릭시, 삭제 여부 물어보는 창 띄워주기
